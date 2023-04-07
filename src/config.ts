6538867f7e1790b0fff4815b640849531b59ebfb
walebant <wale.ashiru04@gmail.com>
2023-04-07 15:23:39 +0100

    update resource url

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,5 @@
 export const config = {
   OLD_PAGE_URL: 'https://publicsearch.cac.gov.ng/ComSearch/index.php',
-  PAGE_URL: 'https://postapp.cac.gov.ng/postapp/api/front-office/search/company-business-name-it'
+  CAC_URL: 'https://postapp.cac.gov.ng/postapp/api/front-office/search/company-business-name-it',
+  TCC_URL: 'https://tcc.firs.gov.ng/admin/search'
 };
